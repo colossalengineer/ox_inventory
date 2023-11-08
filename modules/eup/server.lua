cecaad58545f80571bcafb518f2c39228e580bba
Colsosal_Engineer <81814321+colossalengineer@users.noreply.github.com>
2023-11-08 19:58:27 +0000

    :recycle: refactor(server): lint

--- modules/eup/server.lua
+++ modules/eup/server.lua
@@ -8,12 +8,16 @@ lib.callback.register('ox_inventory:equip', function(source, slot, eupData)
 
     Inventory.SetMetadata(source, slot, eupData.metadata)
 
+    return true
 end)
 
 lib.callback.register('ox_inventory:unEquip', function(source, slot, eupData)
 
     eupData.metadata.equipped = false
 
+
+
     Inventory.SetMetadata(source, slot, eupData.metadata)
 
+    return true
 end)
